dd4ebb387023ceabf2080e8204707c0f6f75fca8
Filip Hracek <filiph@google.com>
2015-03-02 19:31:19 +0100

    .

--- lib/game_of_life.dart
+++ lib/game_of_life.dart
@@ -1,3 +1,5 @@
+library game_of_life;
+
 class Point {
   final int x, y;
   const Point(this.x, this.y);
@@ -7,40 +9,40 @@ class Point {
   toString() => "($x, $y)";
 }
 
-Set<Point> neighs(Point p) {
+Set<Point> _neighs(Point p) {
   const dl = const [-1, 0, 1];
   return dl.expand((dx) => dl.map((dy) => p.offset(dx, dy)))
       .where((np) => np != p)
       .toSet();
 }
 
-bool shouldBeAlive(int nc, bool isAlive) =>
+bool _shouldBeAlive(int nc, bool isAlive) =>
     nc == 3 || (nc == 2 && isAlive);
 
 Set<Point> step(Set<Point> board) {
   var next = new Set<Point>();
   board
-      .map((p) => neighs(p))
+      .map((p) => _neighs(p))
       .expand((p) => p)
       .fold({}, (Map<Point,int> map, p) {
         map[p] = map.putIfAbsent(p, () => 0) + 1;
         return map;
       })
       .forEach((Point p, int frq) {
-        if (shouldBeAlive(frq, board.contains(p))) next.add(p);
+        if (_shouldBeAlive(frq, board.contains(p))) next.add(p);
       });
   return next;
 }
 
 
-main() {
-  var glider = [[1, 3], [2, 3], [3, 3], [3, 2], [2, 1]];
-  var board = new Set.from(glider.map((e) => new Point(e[0], e[1])));
-  board = step(board);
-  board = step(board);
-  board = step(board);
-  board = step(board);
-  board = step(board);
-  board = step(board);
-  print(board);
-}
+//main() {
+//  var glider = [[1, 3], [2, 3], [3, 3], [3, 2], [2, 1]];
+//  var board = new Set.from(glider.map((e) => new Point(e[0], e[1])));
+//  board = step(board);
+//  board = step(board);
+//  board = step(board);
+//  board = step(board);
+//  board = step(board);
+//  board = step(board);
+//  print(board);
+//}
